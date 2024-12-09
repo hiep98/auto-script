55c56224ab9c598de4c1773ed24b10dfae2785f9
HIEP NGUYEN <43906825+hiep98@users.noreply.github.com>
2024-12-09 21:10:59 +0700

    Update vm-install-package.sh

--- vm-install-package.sh
+++ vm-install-package.sh
@@ -1,5 +1,8 @@
 # script auto install package for vm
-sudo apt get update && upgrade -y
+# sudo apt get update && upgrade -y
+sudo apt get update -y
+
+
 sudo apt install python3-pip -y
 
 # for kali
