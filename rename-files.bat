9be369cbc99390fe14b51dc1fb9d51a090ffa4ab
HIEP NGUYEN <43906825+hiep98@users.noreply.github.com>
2024-12-23 16:17:20 +0700

    combine file,folder into 1 loop

--- rename-files.bat
+++ rename-files.bat
@@ -1,32 +1,26 @@
 @echo off
 setlocal enabledelayedexpansion
 
-:: Rename files first
-for /f "tokens=* delims=" %%F in ('dir /s /b /a-d /o-n') do (
-    set "fileName=%%~nxF"
-    set "newFileName=!fileName:[@CybeS] -=!"
-    if not "!fileName!"=="!newFileName!" (
-        echo Renaming file: "%%F" to "%%~dpF!newFileName!"
-        ren "%%F" "!newFileName!"
-    )
-)
-
-:: Rename folders iteratively until no changes are left
-:renameFolders
+:: Rename files and folders in one loop
+:renameItems
 set "changesMade=false"
-for /f "tokens=* delims=" %%D in ('dir /s /b /ad /o-n') do (
-    set "folderName=%%~nxD"
-    set "newFolderName=!folderName:[@CybeS] -=!"
-    if not "!folderName!"=="!newFolderName!" (
-        echo Renaming folder: "%%D" to "%%~dpD!newFolderName!"
-        pushd "%%~dpD" >nul
-        ren "%%~nxD" "!newFolderName!"
-        popd >nul
+for /f "tokens=* delims=" %%I in ('dir /s /b /o-n') do (
+    set "itemName=%%~nxI"
+    set "newItemName=!itemName:[@CybeS] -=!"
+    if not "!itemName!"=="!newItemName!" (
+        if exist "%%I\" (
+            pushd "%%~dpI" >nul
+            ren "%%~nxI" "!newItemName!"
+            popd >nul
+        ) else (
+            ren "%%I" "!newItemName!"
+        )
         set "changesMade=true"
     )
 )
 
-if "%changesMade%"=="true" goto renameFolders
+:: Repeat renaming if changes were made
+if "%changesMade%"=="true" goto renameItems
 
 echo All matching names have been processed.
 pause
