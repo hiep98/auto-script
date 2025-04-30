5d5e84cfbf7010cb7c65f9b221f432fa96e7bbfe
HIEP NGUYEN <43906825+hiep98@users.noreply.github.com>
2025-04-30 18:32:17 +0700

    Update rename-files.bat

--- rename-files.bat
+++ rename-files.bat
@@ -1,44 +1,27 @@
 @echo off
 setlocal enabledelayedexpansion
 
-:: Function to rename items (files and folders)
-:renameItems
-
-:: Loop through all files (including subdirectories) that have [@CybeS] in the name
-for /r %%I in (*[@CybeS]*) do (
-    :: Extract the filename (name and extension) from the full path
+:: 1. Rename all files starting with [@CB] - 
+for /r %%I in ("[@CB] - *") do (
     set "itemName=%%~nxI"
-    
-    :: Replace [@CybeS] with -= in the filename
-    set "newItemName=!itemName:[@CybeS] -=!"
-    
-    :: If the filename is different after renaming, perform the renaming
-    if not "!itemName!"=="!newItemName!" (
-        :: Check if the file's directory exists (to prevent errors)
-        if not "%%~dpI"=="" (
-            echo Renaming "%%I" to "%%~dpI!newItemName!"
-            ren "%%I" "!newItemName!"
-        )
+    set "newItemName=!itemName:[@CB] - =!"
+    if /i not "!itemName!"=="!newItemName!" (
+        echo Renaming file "%%I" to "%%~dpI!newItemName!"
+        ren "%%I" "!newItemName!"
     )
 )
 
-:: Loop through all directories (including subdirectories) that have [@CybeS] in the name
-for /d /r %%I in (*[@CybeS]*) do (
-    :: Extract the folder name from the full directory path
-    set "itemName=%%~nxI"
-    
-    :: Replace [@CybeS] with -= in the folder name
-    set "newItemName=!itemName:[@CybeS] -=!"
-    
-    :: If the folder name is different after renaming, perform the renaming
-    if not "!itemName!"=="!newItemName!" (
-        :: Use pushd to change the directory before renaming the folder
-        pushd "%%~dpI" >nul
-        echo Renaming folder "%%I" to "%%~dpI!newItemName!"
-        ren "%%~nxI" "!newItemName!"
-        popd >nul 
+:: 2. Rename directories bottom-up
+for /f "delims=" %%D in ('dir /b /s /ad "[@CB] - *" ^| sort /r') do (
+    set "itemName=%%~nxD"
+    set "newItemName=!itemName:[@CB] - =!"
+    if /i not "!itemName!"=="!newItemName!" (
+        echo Renaming folder "%%D" to "%%~dpD!newItemName!"
+        pushd "%%~dpD" >nul
+        ren "%%~nxD" "!newItemName!"
+        popd >nul
     )
 )
 
-echo All matching names have been processed.
+echo All matching items have been renamed.
 pause
